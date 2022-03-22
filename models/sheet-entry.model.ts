327d3ba8d319076f65a4983348f7c0e030cb2b77
Radu-Mihai Sîrbu <raduftw01@gmail.com>
2022-03-22 16:16:43 +0200

    added services and BE endpoints

---
+++ models/sheet-entry.model.ts
@@ -0,0 +1,10 @@
+export interface SheetEntry {
+    inDate: string, 
+    details: string,
+    sex: string,
+    zoneName: string,
+    responsible: string,
+    outDate: string,
+    observations: string,
+    media: string
+}
