a9e92bf745175449b60e17e3bcffefebb7d3352c
RazvanPop <pop.razvan.rzp@gmail.com>
2022-03-23 12:42:42 +0200

    changes after resolve conflict

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { useUser } from '@auth0/nextjs-auth0';
 import Link from 'next/link';
-import Welcome from './components/Welcome';
+import Welcome from '../components/Welcome';
 const MainApp = () => {
   const { user, error, isLoading } = useUser();
   if (isLoading) {
@@ -10,7 +10,7 @@ const MainApp = () => {
   if (error) {
     return <div>{error.message}</div>;
   }
-      </Head>
+ 
 
   if (user) {
     return (
@@ -26,54 +26,10 @@ const MainApp = () => {
 
   return(
     <Link href="/api/auth/login">Login</Link>
-};
+  )};
+
 
 export default MainApp;
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+         
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
-}
-
-export default Home
 
