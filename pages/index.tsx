ce8275ad5538e1169d50032af7b37e1f9b741ab5
RazvanNasca <nascarazvanalex@gmail.com>
2022-03-23 14:56:41 +0200

    integrate end point in detailed model

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,36 @@
 import { useUser } from '@auth0/nextjs-auth0';
 import Link from 'next/link';
+import DetailsModal from '../components/DetailsModal';
 import Welcome from './components/Welcome';
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { InterestZone } from '../models/zone.model';
+
 
 const MainApp = () => {
   const { user, error, isLoading } = useUser();
+  const [zone, setZone] = useState<any>();
+  const baseURL = "http://localhost:3000/api/interest-zones/";
+  const idTest = "623aff0e6a541cfe8c68308d";
+
+  useEffect(
+    () => {
+      getZone();
+    }, 
+  [])
+
+  const getZone = async () => {
+
+    try {
+      const foundZone = await axios.get(baseURL + idTest);
+      console.log(foundZone.data);
+      setZone(foundZone.data);
+    }
+    catch(error: any) {
+      console.log(error);
+    }
+  }
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -21,6 +48,7 @@ const MainApp = () => {
         <Link href="/api/auth/logout">Logout</Link>
         <br></br>
         <Welcome user={user}/>
+        <DetailsModal zone={zone}/>
       </div>
     );
   }
